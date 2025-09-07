0621965ca2af72f0f8fffdddad7261c34453d4ab
Mohd Shaan <mohdshaan68581@gmail.com>
2025-09-07 14:00:25 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -27,7 +27,7 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
        {
-        protocol: 'https,
+        protocol: 'https',
         hostname: 'i.pravatar.cc',
         port: '',
         pathname: '/**',
